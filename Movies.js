af031154aeca4238d80d575ccf4cc93232e600f5
kblack-git <keithblack@gmail.com>
2023-03-21 10:58:14 -0400

    initial commit

---
+++ Movies.js
@@ -0,0 +1,19 @@
+export default class Movies {
+    constructor (title, year, genre, averageRating) {
+        this.title=title;
+        this.year=year;
+        this.genre=genre;
+        this.averageRating=averageRating;
+
+     }
+    // add movie
+
+    // read movie
+
+    // update movie
+
+    // delete movie
+
+
+
+    }
---
+++ index.js
@@ -0,0 +1,5 @@
+import Movies from './Movies.js';
+import MovieList from './MovieList.js';
+import Viewer from './Viewer.js';
+
+// create 
